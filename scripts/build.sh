2de3dcd9df0f0c0bb62a46ae6aac05f465d6e7be
Wendell-Lin <s950085az@gmail.com>
2025-03-07 21:02:33 +0800

    make cmake find llvm path from /usr/lib/llvm-*

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,29 +4,9 @@ build_dir=build-llvm-$1
 mkdir -p $build_dir && cd $build_dir
 mkdir -p dbtcache troot tcache
 
-if [ $1 == "19" ]; then
-    LLVM_ROOT="/home/wendell/llvm-19"
-    export PATH="$LLVM_ROOT/bin:$PATH"
-    export LD_LIBRARY_PATH="$LLVM_ROOT/lib:$LD_LIBRARY_PATH"
-    export CXXFLAGS="-I$LLVM_ROOT/include"
-    export LDFLAGS="-L$LLVM_ROOT/lib"
-    export LLVM_DIR="$LLVM_ROOT/lib/cmake/llvm"
-    
-    # Force CMake to use LLVM 19
-    CC=clang CXX=clang++ cmake -GNinja -DCMAKE_BUILD_TYPE=Debug \
-        -DLLVM_DIR="$LLVM_ROOT/lib/cmake/llvm" \
-        -DCMAKE_PREFIX_PATH="$LLVM_ROOT" \
-        -DLLVMConfig_PATH="$LLVM_ROOT/lib/cmake/llvm/LLVMConfig.cmake" \
+CC=clang CXX=clang++ cmake -GNinja -DCMAKE_BUILD_TYPE=Debug \
+        -DLLVM_PATH=/usr/lib/llvm-$1 \
         ..
-else
-    export PATH="/usr/lib/llvm-$1/bin:$PATH"
-    export LD_LIBRARY_PATH="/usr/lib/llvm-$1/lib:$LD_LIBRARY_PATH"
-    export CXXFLAGS="-I/usr/include/llvm-$1"
-    export LDFLAGS="-L/usr/lib/llvm-$1/lib"
-    export LLVM_DIR="/usr/lib/llvm-$1/lib/cmake"
-    
-    CC=clang CXX=clang++ cmake -GNinja -DCMAKE_BUILD_TYPE=Debug ..
-fi
-ninja
 
+ninja
 
