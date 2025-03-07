327710a840fba335e64c4f96536f64f6a5aa62be
Wendell-Lin <s950085az@gmail.com>
2025-03-07 18:25:11 +0800

    feat: auto build, test, benchmark

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+# Scritps
+This directory collects the scripts to build, test, and benchmark, to avoid polluting the parent directory.
+But to run these scripts, please be at parent diretory like following examples.
+
+## Build
---
+++ scripts/benchmark.sh
@@ -0,0 +1,9 @@
+python3 examples/benchmarks.py \
+    --rvdbt-build-dir $(pwd)/build-llvm-$1 \
+    --libriscv-build-dir /home/wendell/libriscv/emulator \
+    --prebuilts $(pwd)/prebuilts \
+    --riscv32-gcc riscv32-unknown-elf-gcc \
+    --rvdbt-jit \
+    --rvdbt-qcgaot \
+    --rvdbt-llvmaot \
+    --libriscv
